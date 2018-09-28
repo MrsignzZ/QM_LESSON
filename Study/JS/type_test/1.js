e5fe97a201d64f0aab60881c0f7f9b050a26eeae
wrr <1440806960@qq.com>
2018-09-28 16:22:48 +0800

    JS基本数据类型理解

---
+++ Study/JS/type_test/1.js
@@ -0,0 +1,37 @@
+//简单类型
+var und = undefined;
+var nul = null;
+var boo = true;
+var num = 1;
+var str = 'xys';
+//Symbol
+
+//复杂数据类型 引用类型
+var obj = {a: 1};
+var func = function () {
+
+}
+var arr = [1,2,3];
+//引用式复制
+// var obj1 = obj;
+
+// var obj1 = {};
+// for(var key in obj){
+//   obj1[key] = obj[key];
+// }
+// obj1.a = 2;
+// console.log(obj1);
+// console.log("----------");
+// console.log(obj);
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof und);
+console.log(typeof nul);
+console.log(typeof boo);
+console.log(typeof func);
+
+
+
+
+
