7a9f796c2fea8de6ee549a5e4fe5becc1981e94c
1440806960@qq.come <1440806960@qq.com>
2018-10-19 22:17:51 +0800

    babel

---
+++ Study/小程序/weather/es5.js
@@ -0,0 +1,12 @@
+'use strict';
+
+var num = 9;
+var isMarried = false;
+
+var yourTurn = ['好工作', '找老板谈', '驴妈妈网'].map(function (task, index) {
+  return index + '-' + task;
+});
+
+console.log(yourTurn);
+
+// es6 => es5
