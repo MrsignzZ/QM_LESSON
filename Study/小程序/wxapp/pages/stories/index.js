b9e88b4a683138a47593da6ea762332537e80577
1440806960@qq.come <1440806960@qq.com>
2018-10-22 22:55:54 +0800

    video解决

--- Study/小程序/wxapp/pages/stories/index.js
+++ Study/小程序/wxapp/pages/stories/index.js
@@ -2,8 +2,26 @@ const app = getApp()
 
 Page({
   data: {
-
+    stories: [],
+    currentVid: null,
+    currentVideo: null
   },
   //生命周期
-  
+  onLoad(options) {
+    this.setData({
+      stories: app.globalData.stories
+    })
+  },
+  play(event) {
+    if(this.data.currentVideo !== null) {
+      this.data.currentVideo.pause();
+    }
+    const vid = event.currentTarget.dataset.vid;
+    const currentVideo = wx.createVideoContext(`${vid}`);
+    currentVideo.play();
+    this.setData({
+      currentVideo,
+      currentVid: vid
+    })
+  }
 });
