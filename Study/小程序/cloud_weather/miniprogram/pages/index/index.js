e2ef71402b5eac6f7af423279240898f54fdb230
1440806960@qq.come <1440806960@qq.com>
2018-10-23 22:56:20 +0800

    微信小程序-云开发

---
+++ Study/小程序/cloud_weather/miniprogram/pages/index/index.js
@@ -0,0 +1,48 @@
+wx.cloud.init({
+  env: 'codingdream-74b4e5'
+})
+
+const db = wx.cloud.database()
+const _ = db.command
+const productsCollection = db.collection('products')
+
+Page({
+  onPullDownRefresh: function () {
+    productsCollection.get().then(res => {
+      this.setData({
+        products: res.data
+      }, res =>{
+        console.log('数据更新完成')
+        wx.stopPullDownRefresh()
+      })
+    })
+  },
+  onReachBottom: function () {
+    console.log('上拉加载更多');
+  },
+  data: {
+    page: 1,
+    products: []
+  },
+  onLoad: function () {
+    productsCollection
+      .get()
+      .then(res => {
+        this.setData({
+          products: res.data
+        })
+        // console.log(res.data);
+      })
+  },
+  click: function (event) {
+    // update view + 1 doc  一条记录
+    // database<-collection<-document
+    productsCollection.doc(
+      event.currentTarget.dataset.id)
+      .update({
+        data: {
+          view: _.inc(1)
+        }
+      })
+  }
+})
---
+++ Study/小程序/cloud_weather/miniprogram/pages/readme.md
@@ -0,0 +1,6 @@
+- **BASS** <br>
+Backend as service<br>
+后端开发变成一种服务，在云开发的时代，时间是王道，js工程师已经不满足于用node.js走后端，服务器购买，linux配置，数据库及ngnix管理，运维这边高花销且不易处理，打包给我们用。<br>
+后端 sdk wx.cloud 数据库mongodb 文件上传 用户登录 内置<br>
+全栈 一个人从前到后全搞定，得益于BASS<br>
+向前端工程师示好，简单方便，快速出活
