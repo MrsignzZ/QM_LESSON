f95e6ea02dde027c257260bd02efd59540269837
1440806960@qq.come <1440806960@qq.com>
2018-10-23 12:36:01 +0800

    完善了video播放的问题

--- Study/小程序/wxapp/pages/stories/index.js
+++ Study/小程序/wxapp/pages/stories/index.js
@@ -1,4 +1,4 @@
-const app = getApp()
+const app = getApp();
 
 Page({
   data: {
@@ -9,11 +9,13 @@ Page({
   //生命周期
   onLoad(options) {
     this.setData({
+      video_src:
+        'http://wxsnsdy.tc.qq.com/105/20210/snsdyvideodownload?filekey=30280201010421301f0201690402534804102ca905ce620b1241b726bc41dcff44e00204012882540400&bizid=1023&hy=SH&fileparam=302c020101042530230204136ffd93020457e3c4ff02024ef202031e8d7f02030f42400204045a320a0201000400',
       stories: app.globalData.stories
-    })
+    });
   },
   play(event) {
-    if(this.data.currentVideo !== null) {
+    if (this.data.currentVideo !== null) {
       this.data.currentVideo.pause();
     }
     const vid = event.currentTarget.dataset.vid;
@@ -22,6 +24,6 @@ Page({
     this.setData({
       currentVideo,
       currentVid: vid
-    })
+    });
   }
 });
