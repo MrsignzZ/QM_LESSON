ceed19f187fbfffab3ddc9a1100fc61a28549cfc
1440806960@qq.come <1440806960@qq.com>
2018-10-31 22:22:31 +0800

    完善了search框和swiper的样式，完成了content_h1部分swiper样式

--- Study/小程序/cloud_yanxuan/miniprogram/pages/index/index.js
+++ Study/小程序/cloud_yanxuan/miniprogram/pages/index/index.js
@@ -1,37 +1,27 @@
 wx.cloud.init({});
-const db = wx.cloud.database();
+const AdsInfo = wx.cloud.database();
+const CollageInfo = wx.cloud.database();
 Page({
   data: {
+    collageInfo: [],
     ads: [],
-    servicePolicy: [
-      {
-        text: "网易自营品牌"
-      },
-      {
-        text: "30天无忧退货"
-      },
-      {
-        text: "48小时快速退款"
-      }
-    ]
+      
   },
   onLoad: function() {
-    db.collection('ads').get({
+    AdsInfo.collection('ads').get({
       success: res => {
         this.setData({
           ads: res.data
         });
-        wx.cloud.callFunction({
-          name: 'getTravelInfo',
-          data: {
-            count: 5,
-            startIndex: 0
-          },
-          success: res => {
-            console.log(res);
-          }
-        });
+
       }
     });
+  CollageInfo.collection('collageInfo').get({
+    success: res => {
+      this.setData({
+        collageInfo: res.data
+      })
+    }
+  })
   }
 });
