a1a5360a93556b85570c3076e8103ce1ab49ce9b
1440806960@qq.come <1440806960@qq.com>
2018-11-12 09:24:47 +0800

    readFileSync

--- Study/node/fs/1.js
+++ Study/node/fs/1.js
@@ -1,8 +1,56 @@
-var fs = require('fs');
-var data;
+const fs = require('fs');
+
 try {
-  data = fs.readFileSync('./fileForRead.txt', 'utf8');
-  console.log(data);
-} catch (err) {
-  console.error('读取文件出错: '+ err.message);
+  // Promise也可以，then链
+  const fileAPromise = new Promise((resolve, reject) => {
+    fs.readFile('./a.txt', 'utf8', function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+  const fileBPromise = new Promise((resolve, reject) => {
+    fs.readFile('./b.txt', 'utf8', function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+  const fileCPromise = new Promise((resolve, reject) => {
+    fs.readFile('./c.txt', 'utf8', function(err, data) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    })
+  })
+  Promise.all([fileAPromise, fileBPromise, fileCPromise])
+    .then(res => {
+      console.log(res)
+    })
+
+  // const dataA = fs.readFileSync('./a.txt', 'utf8');
+  // const dataB = fs.readFileSync('./b.txt', 'utf8');
+  // const dataC = fs.readFileSync('./c.txt', 'utf8');
+} catch (e) {
+  console.log('读取文章失败' + e.message);
 }
+// try {
+//   fs.readFile('./a.txt', 'utf8', function (err, data) {
+//     console.log(data)
+//     fs.readFile('/b.txt', 'utf8', function (err, data) {
+//       console.log(data)
+//       fs.readFile('/c.txt', 'utf8', function (err, data) {
+//         console.log(data)
+//       })
+//     })
+//   })
+
+// } catch (e) {
+
+// }
