79249ce54366c5c6333bae26972d82cbc4ed7123
1440806960@qq.come <1440806960@qq.com>
2018-12-09 10:55:51 +0800

    react antd UI框架

---
+++ Study/react/antd-demo/README.md
@@ -0,0 +1,9 @@
+1. 一切
+2. 路由接管一切，配置在最前面<br>
+`
+<Router>
+  <Route path="" component="">
+</Router>
+`<br>
+有两种路由 BrowserRouter(高级浏览器 mobile, URL 更好理解) HashRouter(兼容性好)<br>
+history.pushState(state, title, url)
---
+++ Study/react/antd-demo/src/Beer.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import Result from './Results';
+import Search from './Search';
+export default class Beer extends Component {
+  //当组件的基本结构加载到页面之后再加载数据
+  componentDidMount() {
+    this.loadBeers();
+  }
+
+  componentDidUpdate(prevProps) {
+    const currentSearchTerm = this.props.match.params.searchTerm;
+    const oldSearchTerm = prevProps.match.params.searchTerm;
+    if (currentSearchTerm !== oldSearchTerm) {
+      this.loadBeers(currentSearchTerm);
+    }
+    console.log(currentSearchTerm);
+    // this.loadBeers(currentSearchTerm);
+  }
+  state = {
+    beers: [],
+    loading: true
+  };
+  loadBeers = (searchTerm = 'hops') => {
+    this.setState({
+      loading: true
+    });
+    fetch(`http://api.react.beer/v2/search?q=${searchTerm}&type=beer`)
+      .then(data => data.json())
+      .then(data => {
+        // console.log(data)
+        const beers = data.data || []
+        this.setState({
+          beers: data.data,
+          loading: false
+        });
+      })
+      .catch(err => console.error(err));
+  };
+  render() {
+    return (
+      <div className="wrapper">
+        <Search />
+        <Result beers={this.state.beers} loading={this.state.loading} />
+      </div>
+    );
+  }
+}
