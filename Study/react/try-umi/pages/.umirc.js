47507e57edc1aefd3fb251ccace815d18fecf383
1440806960@qq.come <1440806960@qq.com>
2019-01-08 10:50:47 +0800

    react umi

---
+++ Study/react/try-umi/pages/.umirc.js
@@ -0,0 +1,15 @@
+const path = require('path');
+
+export default {
+  exportStatic: {
+    htmlSuffix: true
+  },
+  plugins: [
+    [
+      'umi-plugin-react',
+      {
+        hd: true
+      }
+    ]
+  ]
+};
