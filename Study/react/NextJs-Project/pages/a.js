6564b63eeabe99159f8c7af3a778c8518d52c382
MrsignzZ <1440806960@qq.com>
2019-06-26 08:45:11 +0800

    [update] update router hook

--- Study/react/NextJs-Project/pages/a.js
+++ Study/react/NextJs-Project/pages/a.js
@@ -1,6 +1,9 @@
 import { withRouter } from "next/router";
+import Link from "next/link";
 import Comp from '../components/comp'
 
-const A = ({ router }) => <Comp>A{router.query.id}</Comp>
+const A = ({ router }) => <Link href="#aaa">
+  <a>A{router.query.id}</a>
+</Link>
 
 export default withRouter(A)
--- Study/react/NextJs-Project/pages/index.js
+++ Study/react/NextJs-Project/pages/index.js
@@ -2,6 +2,25 @@ import Link from "next/link";
 import Router from 'next/router'
 import { Button } from 'antd'
 
+const events = [
+  'routeChangeStart',
+  'routeChangeComplete',
+  'routeChangeError',
+  'beforeHistoryChange',
+  'hashChangeStart',
+  'hashChangeComplete'
+]
+
+function makeEvent(type) {
+  return (...args) => {
+    console.log(type, ...args);
+  }
+}
+
+events.forEach(event => {
+  Router.events.on(event, makeEvent(event))
+})
+
 export default () => {
 
   function gotoTestB(params) {
