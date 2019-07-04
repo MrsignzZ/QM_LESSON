1212b6bd7743ffbd67d0e35318c8afcf9f3e4ee4
MrsignzZ <1440806960@qq.com>
2019-07-04 08:49:30 +0800

    [update] add hooks

--- Study/react/NextJs-Project/pages/_app.js
+++ Study/react/NextJs-Project/pages/_app.js
@@ -2,8 +2,12 @@ import App, { Container } from 'next/app'
 import 'antd/dist/antd.css'
 import Layout from '../components/layout'
 
+import MyContext from '../lib/my-context'
 class MyApp extends App {
 
+  state ={
+    context: 'value'
+  }
   static async getInitialProps({ Component, ctx }) {
     console.log('app init')
     let pageProps = {}
@@ -23,7 +27,10 @@ class MyApp extends App {
     return (
       <Container>
         <Layout>
-          <Component {...pageProps}/>
+          <MyContext.Provider value={this.state.context}>
+            <Component {...pageProps}/>
+            <button onClick={() => this.setState({ context: `${this.state.context}new` })}>update context</button>
+          </MyContext.Provider>
         </Layout>
       </Container>
     )
--- Study/react/NextJs-Project/pages/test/b.js
+++ Study/react/NextJs-Project/pages/test/b.js
@@ -1,2 +1,58 @@
+import { useState, useReducer, useLayoutEffect, useContext, useRef, useEffect } from "react";
 
-export default () => <span> B </span>
+import MyContext from '../../lib/my-context'
+
+function countReducer(state, action) {
+  switch (action.type) {
+    case 'add':
+      return state + 1
+    case 'minus':
+      return state -1
+    default:
+      return state
+  }
+}
+
+function MyCountFunc() {
+  // const [count, setCount] = useState(0)
+
+  const [ count, dispatchCount] = useReducer(countReducer, 0)
+  const [name, setName] = useState('MrsignzZ')
+
+  const context = useContext(MyContext)
+  const inputRef = useRef()
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     dispatchCount( { type: 'add' } )
+  //   }, 1000)
+  //   return () => clearInterval(interval)
+  // }, [])
+  useEffect(() => {
+    console.log('effect invoked');
+
+    return () => {
+      console.log('effect deteched');
+      console.log(inputRef);
+      
+    };
+  }, [count])
+
+  useLayoutEffect(() => {
+    console.log('layouteffect invoked');
+
+    return () => {
+      console.log('layouteffect deteched');
+    };
+  }, [count])
+
+  // return <span>{count}</span>
+  return (
+    <div>
+      <input ref={inputRef} value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => dispatchCount({ type: 'add' })}>{count}</button>
+      <p>{context}</p>
+    </div>
+  )
+}
+export default MyCountFunc
